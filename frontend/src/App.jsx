ebafa14f74671b1f39d389e254d92d3a93985905
Wasiq Tanveer <mwasiqt@gmail.com>
2026-08-09 13:57:45 +0500

    add react router setup with login, signup, dashboard routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import {Routes, Route, Link, Navigate} from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+
+
+
+function App()
+{
+  return(
+    <Routes>
+      <Route path='/' element={<Navigate to='/login'/>}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/signup' element={<Signup />}/>
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Routes>
+  )
+}
+
+export default App;
